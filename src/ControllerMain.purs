3840327c8e2e99cce49a3cfd2d2277191ab6254f
McKay Broderick <mckay.db@gmail.com>
2017-06-03 15:56:41 -0600

    Removed unused elements until later.

--- src/ControllerMain.purs
+++ src/ControllerMain.purs
@@ -5,17 +5,9 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import AirConsole.Global ( getAirConsoleGlobal
                          , orientationPortrait
-                         , screen
-                         , onMessage
-                         , onActivePlayersChange
                          )
-import AirConsole.ControllerInputs (vibrate)
 import Views.ControllerStart (view)
-import Views.FFI (renderToSel, onDOMContentLoaded)
-
-import Text.Smolder.HTML (button)
-import Text.Smolder.Markup (text, on, (#!))
-import Text.Smolder.Renderer.String (render)
+import Views.FFI (onDOMContentLoaded)
 import DOM (DOM)
 
 foreign import isNullOrUndefined :: forall a. a -> Boolean
@@ -24,13 +16,4 @@ main :: forall e. Eff (dom :: DOM, console :: CONSOLE | e) Unit
 main = onDOMContentLoaded do
   ac <- getAirConsoleGlobal { orientation: orientationPortrait }
   view ac
-  log "Done!"
-  -- _ <- onMessage
-        -- (\from d -> if d.vibrate && from == screen
-                    -- then vibrate ac 1000
-                    -- else pure unit) ac
-  -- _ <- onActivePlayersChange
-        -- (\d -> if isNullOrUndefined d
-               -- then renderToSel ".player_id" "It's a 2 player game!"
-               -- else renderToSel ".player_id" ("Player " <> show d)) ac
-  -- log "Controller Ready"
+  log "Controller Ready!"
