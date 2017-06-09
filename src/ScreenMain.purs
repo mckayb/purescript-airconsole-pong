c305675ffa332d4fb3fb4e01dd525eaf962779ee
McKay Broderick <mckay.db@gmail.com>
2017-06-09 07:50:19 -0600

    Got the essentials of the onConnect, onDisconnect, and onMessage set up

--- src/ScreenMain.purs
+++ src/ScreenMain.purs
@@ -1,17 +1,22 @@
 module AirConsolePong.ScreenMain where
 
-import Prelude (Unit, discard, bind, pure, unit, (<$>))
+import Prelude (Unit, discard, bind, pure, unit, (<$>), (<<<), (==), (>=), (&&))
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Eff.Timer (TIMER)
-import AirConsole.Global ( getAirConsoleGlobal , orientationLandscape
-                         , onConnect , onReady , onMessage , onDisconnect
+import AirConsole.Global ( getAirConsoleGlobal, orientationLandscape
+                         , onConnect, onMessage, onDisconnect
                          )
-import AirConsolePong.Views.FFI (updateCanvasDim)
+import AirConsole.Types (AirConsoleGlobal, DeviceId)
+import AirConsole.ActivePlayers (convertDeviceIdToPlayerNumber, getActivePlayerDeviceIds, setActivePlayers)
+import AirConsole.Connectivity (getControllerDeviceIds)
+import AirConsolePong.Views.FFI (updateCanvasDim, showStuff)
 import AirConsolePong.Views.ScreenStart (view , drawGame)
 import AirConsolePong.Game.Model (initialGameState)
 import AirConsolePong.Game.Update (gameLogic, Input)
 import Data.Maybe (Maybe(Just, Nothing))
+import Data.Nullable (toMaybe)
+import Data.Array (length)
 import DOM (DOM)
 import Graphics.Canvas (CANVAS, getCanvasElementById)
 import Signal (foldp, runSignal, sampleOn)
@@ -38,6 +43,73 @@ startGame channel = do
             pure unit
         Nothing -> pure unit
 
+handleConnection
+    :: forall e
+     . AirConsoleGlobal
+    -> Channel Input
+    -> DeviceId
+    -> Eff ( dom :: DOM
+           , timer :: TIMER
+           , console :: CONSOLE
+           , canvas :: CANVAS
+           | e ) Unit
+handleConnection ac ch d =
+    let
+        apLength :: Int
+        apLength = (length <<< getActivePlayerDeviceIds) ac
+
+        cdLength :: Int
+        cdLength = (length <<< getControllerDeviceIds) ac
+     in
+        if apLength == 0 && cdLength >= 2
+           then do
+               _ <- setActivePlayers ac 2
+               startGame ch
+        else if apLength == 0 && cdLength == 1
+            then do
+               pure unit
+        else if apLength == 0 && cdLength == 0
+            then do
+                pure unit
+        else do
+            pure unit
+
+handleDisconnect
+    :: forall e
+     . AirConsoleGlobal
+    -> Channel Input
+    -> DeviceId
+    -> Eff ( dom :: DOM
+           , timer :: TIMER
+           , console :: CONSOLE
+           , canvas :: CANVAS
+           | e ) Unit
+handleDisconnect ac ch d = do
+    mpn <- pure ((toMaybe <<< convertDeviceIdToPlayerNumber ac) d)
+    case mpn of
+         Just pn -> do
+             _ <- setActivePlayers ac 0
+             pure unit
+         Nothing -> pure unit
+    handleConnection ac ch d
+
+handleMessage
+    :: forall e a
+     . AirConsoleGlobal
+    -> Channel Input
+    -> DeviceId
+    -> { move :: Number | a }
+    -> Eff (console :: CONSOLE, channel :: CHANNEL | e) Unit
+handleMessage ac ch d x = do
+    mpn <- pure ((toMaybe <<< convertDeviceIdToPlayerNumber ac) d)
+    case mpn of
+         Just pn -> showStuff pn
+         Nothing -> log "Nothing"
+    send ch { p1: { move: x.move }
+            , p2: { move: 0.0 }
+            , ball: { x: 0.0, y: 0.0 }
+            }
+
 main
     :: forall e
      . Eff ( channel :: CHANNEL
@@ -53,15 +125,9 @@ main = do
                   , ball: { x: 0.0, y: 0.0 }
                   }
     view ac
-    _ <- onConnect (\d -> log "on Connection") ac
-    _ <- onMessage (\d x -> do
-                        send ch { p1: { move: x.move }
-                                , p2: { move: 0.0 }
-                                , ball: { x: 0.0, y: 0.0 }
-                                }
-                   ) ac
-    _ <- onReady (\d -> log "on Ready") ac
-    _ <- onDisconnect (\d -> log "on Disconnect") ac
+    _ <- onConnect (handleConnection ac ch) ac
+    _ <- onMessage (handleMessage ac ch) ac
+    _ <- onDisconnect (handleDisconnect ac ch) ac
     startGame ch
     log "Running"
 
